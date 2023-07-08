f45a726bc564773e7f9a91fc3ad5e9f32451dbee
root <danielkay62@gmail.com>
2023-07-08 05:10:52 -0700

    A puppet script that create a file in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-# Using Puppet to create a file in /tmp
 file { '/tmp/school':
-  path    => '/tmp/school',
+  ensure  => file,
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'Ilove Puppet',
 }
