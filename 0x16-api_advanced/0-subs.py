f1a40060abbcd9217e776752d765996396e36572
dannykay62 <danielkay62@gmail.com>
2023-08-08 16:29:30 +0100

    queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-"""queries the Reddit API and returns the number of subscribers"""
+"""
+queries the Reddit API and returns the number of subscribers
+"""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "user-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/dan)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Python/3.0 (ALX 0x16 task 0)"}
+
+    try:
+        response = requests.get(url, headers=headers)
+        data = response.json()
+
+        if "data" in data and "subscribers" in data["data"]:
+            return data["data"]["subscribers"]
+        else:
+            return 0
+
+    except requests.executions.RequestException as e:
+        printf(f"Error: {e}")
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+
+    subreddt_name = "python"
+    subscribers = name_of_subscribers(subreddit_name)
+    print(f"Number of subscribers in r/{subreddit_name}: {subscribers}")
